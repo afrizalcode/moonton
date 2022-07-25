dc9567567611b42b68b1a3e0d28cf2729b7c7dd7
afrizalcode <afrizalmarzuki5@gmail.com>
2022-07-25 20:29:13 +0700

    feat: slicing subscription card2

--- resources/js/Components/FeatureMovie.jsx
+++ resources/js/Components/FeatureMovie.jsx
@@ -1,3 +1,4 @@
+import { Link } from "@inertiajs/inertia-react";
 import PropTypes from "prop-types";
 
 FeatureMovie.propTypes = {
@@ -42,7 +43,7 @@ export default function FeatureMovie({
                     <img src="/icons/ic_play.svg" width="50" alt="" />
                 </div>
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link href={route('prototype.movie.show', slug)} className="inset-0 absolute z-50"></Link>
         </div>
     );
 }
