11d046e824718044cb2ad2da7317ccca1e2e937e
afrizalcode <afrizalmarzuki5@gmail.com>
2022-08-02 06:07:11 +0700

    feat: middleware check user subscription

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+
+        'checkUserSubscription' => \App\Http\Middleware\CheckUserSubscription::class,
     ];
 }
