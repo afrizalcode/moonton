926c1bcf598b7155d88d7550d7eb5db35f1ab62a
afrizalcode <afrizalmarzuki5@gmail.com>
2022-08-02 02:21:39 +0700

    feat: integrate sidebar

---
+++ app/Http/Controllers/User/SubscriptionPlanController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\SubscriptionPlan;
+use App\Models\UserSubscription;
+use Auth;
+use Carbon\Carbon;
+
+class SubscriptionPlanController extends Controller
+{
+    public function index()
+    {
+        
+        // cek data
+        $subcriptionplan = SubscriptionPlan::all();
+        // return $subcriptionplan;
+
+
+        return inertia('User/Dashboard/SubscriptionPlan/Index', [
+            'subscriptionPlan' => SubscriptionPlan::all(),
+        ]);
+    }
+
+    public function userSubscribe(Request $request, SubscriptionPlan $subscriptionPlan)
+    {
+        $data = 
+        [
+            'user_id' => Auth::id(),
+            'subscription_plan_id' => $subscriptionPlan->id,
+            'price' => $subscriptionPlan->price,
+            'expired_date' => Carbon::now()->addMonths($subscriptionPlan->active_period_in_months),
+            'payment_status' => 'paid'
+        ];
+
+        // cek isi variable data
+        // return $data;
+
+        $userSubscription = UserSubscription::create($data);
+
+        return redirect(route('user.dashboard.index'));
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\User\DashboardController;
 use App\Http\Controllers\User\MovieController;
+use App\Http\Controllers\User\SubscriptionPlanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,10 @@ Route::middleware('auth', 'role:user')->prefix('dashboard')->name('user.dashboar
     Route::get('/', [DashboardController::class, 'index'])->name('index');
 
     Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show');
+
+    Route::get('subscription-plan', [SubscriptionPlanController::class, 'index'])->name('subscriptionPlan.index');
+
+    Route::post('subscription-plan/{subscriptionPlan}/user-subscribe', [SubscriptionPlanController::class, 'userSubscribe'])->name('subscriptionPlan.userSubscribe');
 
 });
 
